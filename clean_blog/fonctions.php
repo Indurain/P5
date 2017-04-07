73c7d15e8413515c484628053b3f22e09861fbc2
kanazawa <carmenfabo@gmail.com>
2017-04-07 12:59:34 +0200

    added fonctions.php with conexion and count articles fonctions

---
+++ clean_blog/fonctions.php
@@ -0,0 +1,29 @@
+<?php
+
+function connexion() {
+   try
+   {
+      $dsn = 'mysql:host=localhost; dbname=P5; charset=utf8';
+      $username='root';
+      $password='';
+      
+      $bdd = new PDO($dsn, $username, $password);
+      $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+      $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+   }
+
+   catch(Exception $e)
+   {
+      die('Erreur : '.$e->getMessage());
+   }
+
+   return $bdd;
+}
+
+function compter_articles($bdd) {
+   $sql = "SELECT COUNT(id_article) AS nbArt FROM article";
+   $req = $bdd->query($sql);
+   $data = $req->fetch();
+   $data2 = $data['nbArt'];
+   return $data2; 
+}
