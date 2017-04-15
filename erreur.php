1cfc42ea762c0062225be0ea93650399a9aa1cdd
kanazawa <carmenfabo@gmail.com>
2017-04-15 16:58:01 +0200

    fix error page

--- erreur.php
+++ erreur.php
@@ -1,5 +1,85 @@
-<?php
-echo "<a href=\"index.php\">";
-echo "<img src=\"img/perplejidad.jpg\"/>";
-echo "</a>";
-?> 
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Carmen Fabo Indurain</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="freelancer/css/freelancer.min.css" rel="stylesheet">
+    <link href="freelancer/css/carmen.css" rel="stylesheet">
+    
+    <!-- Carmen css pour page d'erreur-->
+    <link href="style.css" rel="stylesheet">
+    
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-text">
+                        <span class="name">
+                            Carmen Fabo Indurain
+                        </span>
+                        <hr class="star-light">
+                        <span class="skills">Web Developer - PHP / Symfony</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+    <p id="message_erreur"> <a href="http://www.openclassrooms.carmen-fabo.com/P5/freelancer/index.php">Page d'erreur : il semblerait qu'une erreur s'est produit. Vous pouvez revenir à la page d'accueil en clicant sur ce message.</a></p>
+    </section>
+
+    
+    
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="freelancer/js/jqBootstrapValidation.js"></script>
+    <script src="freelancer/js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="freelancer/js/freelancer.min.js"></script>
+
+</body>
+
+</html>
