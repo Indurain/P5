905132e2dbe9367d3f18d67b2bff25fceaf58dcc
kanazawa <carmenfabo@gmail.com>
2017-04-20 12:10:34 +0200

    fix navigation bar problem

--- clean_blog/nav.php
+++ clean_blog/nav.php
@@ -1,10 +1,15 @@
 <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container-fluid">
-        <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
             <div class="navbar-header page-scroll">
                 <a class="navbar-brand" href="index.php">Carmen Fabo, le blog</a>
             </div>
-        </div>
+            
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
